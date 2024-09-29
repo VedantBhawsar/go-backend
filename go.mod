bd719751e695839e55e07f0ef9fad1b26fcab57e
Vedant bhavsar <vedantbhavsar.a10@gmail.com>
2024-09-29 10:24:03 +0530

    completed

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module user-crud
 go 1.23.1
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/sirupsen/logrus v1.9.3
 )
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-
-	"net/http"
-	"time"
+	"io/ioutil"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 type User struct {
+	Id    int    `json:"id"`
 	Name  string `json:"name"`
 	Age   int    `json:"age"`
 	Email string `json:"email"`
@@ -60,30 +62,104 @@ func loggingMiddleware(next http.Handler) http.Handler {
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getUsers")
 	json.NewEncoder(w).Encode(users)
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("createUser")
-	// Logic to create a user should be added here
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to read body", http.StatusBadRequest)
+		return
+	}
+
+	var user User
+	var userLength int = len(users)
+	user.Id = userLength + 1
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		http.Error(w, "Unable to unmarshal body", http.StatusBadRequest)
+		return
+	}
+	users = append(users, user)
+
 	json.NewEncoder(w).Encode(users)
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getUser")
-	// Logic to retrieve a specific user by ID should be added here
-	json.NewEncoder(w).Encode(users)
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	fmt.Println(id)
+	for _, user := range users {
+		if userId, err := strconv.Atoi(id); err == nil && user.Id == userId {
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("User not found")
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("updateUser")
-	// Logic to update a user should be added here
-	json.NewEncoder(w).Encode(users)
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to read body", http.StatusBadRequest)
+		return
+	}
+
+	// Temporary struct with unexported fields
+	type Temp struct {
+		Name  *string `json:"name"`
+		Age   *int    `json:"age"`
+		Email *string `json:"email"`
+	}
+
+	var user1 Temp
+	fmt.Printf("Request Body: %s\n", string(body))
+	err = json.Unmarshal(body, &user1)
+	if err != nil {
+		http.Error(w, "Unable to unmarshal body", http.StatusBadRequest)
+		return
+	}
+
+	for index, user := range users {
+		if userId, err := strconv.Atoi(id); err == nil && user.Id == userId {
+			if user1.Email != nil {
+				users[index].Email = *user1.Email
+			}
+			if user1.Age != nil {
+				users[index].Age = *user1.Age
+			}
+			if user1.Name != nil {
+				users[index].Name = *user1.Name
+			}
+			json.NewEncoder(w).Encode(users[index])
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("User not found")
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("deleteUser")
-	// Logic to delete a user should be added here
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var error = true
+	for index, user := range users {
+		if userId, err := strconv.Atoi(id); err == nil && user.Id == userId {
+			users = append(users[:index], users[index+1:]...)
+			error = false
+		}
+	}
+	if error {
+		json.NewEncoder(w).Encode("User not found")
+		return
+	}
 	json.NewEncoder(w).Encode(users)
+	return
 }
